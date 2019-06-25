e8ab6047a0e21d7ea9a2d0edfaa7766a265e8135
Simon Ward <substance33@gmail.com>
2019-06-25 15:50:37 +0200

    More and more setup

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
-from distutils.core import setup
-from distutils import util
-
+from setuptools import setup, find_packages
+from distutils.util import convert_path
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 
-pathMySubPackage1 = util.convert_path('pySpinW/swFuncs')
-pathMySubPackage1data = util.convert_path('pySpinW/swFuncs/*.ctf')
+pathMySubPackage1 = convert_path('pySpinW/swFuncs')
+pathMySubPackage1data = convert_path('pySpinW/swFuncs/*.ctf')
 
 setup(
      name='pySpinW',
@@ -17,13 +16,12 @@ setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/SpinW/pySpinW",
-    package_dir={
-        'pySpinW': 'pySpinW',
-        'pySpinW.swFuncs': pathMySubPackage1},
-    package_data={'pySpinW.swFuncs': [pathMySubPackage1data]},
+     packages=find_packages(),
+     package_data={'swFuncs': [pathMySubPackage1data]},
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
      ],
+     include_package_data=True,
  )
