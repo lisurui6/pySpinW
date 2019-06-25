b1cec72eb83ea75d9cb5e1a0d406bbc40518e890
Simon Ward <substance33@gmail.com>
2019-06-25 15:20:32 +0200

    Repackage and hope

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
-from setuptools import find_packages, setup
-
+from distutils.core import setup
+from distutils import util
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+
+pathMySubPackage1 = util.convert_path('pySpinW/swFuncs')
+pathMySubPackage1data = util.convert_path('pySpinW/swFuncs/*.ctf')
 
 setup(
      name='pySpinW',
@@ -14,8 +17,10 @@ setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/SpinW/pySpinW",
-     packages = find_packages(),
-     package_data={'swFuncs': ['*.ctf']},
+    package_dir={
+        'pySpinW': 'pySpinW',
+        'pySpinW.swFuncs': pathMySubPackage1},
+    package_data={'pySpinW.swFuncs': pathMySubPackage1data},
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
