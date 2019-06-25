5773a63ffa841325881a3feb952f3eeaa9acc0fb
Simon Ward <substance33@gmail.com>
2019-06-25 15:07:52 +0200

    more setup fun :-/

--- setup.py
+++ setup.py
@@ -1,19 +1,9 @@
+from setuptools import find_packages, setup
 
-from distutils.core import setup
-from pkgutil import walk_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-
-def find_packages(path=__path__, prefix=""):
-    yield prefix
-    prefix = prefix + "."
-    for _, name, ispkg in walk_packages(path, prefix):
-        if ispkg:
-            yield name
-
-import pySpinW
 
 setup(
      name='pySpinW',
@@ -24,7 +14,8 @@ setup(
      long_description=long_description,
      long_description_content_type="text/markdown",
      url="https://github.com/SpinW/pySpinW",
-     packages = list(find_packages(pySpinW.__path__, pySpinW.__name__)),
+     packages = find_packages(),
+     package_data={'swFuncs': ['*.ctf']},
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
