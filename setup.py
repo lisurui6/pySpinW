0e58318f191c87608f402364bf17673d25ff8120
Simon Ward <substance33@gmail.com>
2019-06-25 15:02:41 +0200

    modify setup for distutils

--- setup.py
+++ setup.py
@@ -1,18 +1,30 @@
-import setuptools
+
+from distutils.core import setup
+from pkgutil import walk_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+
+def find_packages(path=__path__, prefix=""):
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
+
+import pySpinW
+
+setup(
      name='pySpinW',
      version='0.1',
      author="Simon Ward",
      author_email="simon.ward@esss.se",
      description="Python implementation of SpinW",
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     long_description_content_type="text/markdown",
      url="https://github.com/SpinW/pySpinW",
-     packages=setuptools.find_packages(),
+     packages = list(find_packages(pySpinW.__path__, pySpinW.__name__)),
      classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
