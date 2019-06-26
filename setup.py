cf1d823d8a7d75aa43bfa743dbd6fab79b696e44
Simon Ward <substance33@gmail.com>
2019-06-26 11:24:23 +0200

    Fix install on all media

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
-from setuptools import setup, find_packages
-from distutils.util import convert_path
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-
-pathMySubPackage1 = convert_path('pySpinW/swFuncs')
-pathMySubPackage1data = convert_path('pySpinW/swFuncs/*.ctf')
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup, find_packages
 
 setup(
-     name='pySpinW',
-     version='0.1',
-     author="Simon Ward",
-     author_email="simon.ward@esss.se",
-     description="Python implementation of SpinW",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/SpinW/pySpinW",
-     packages=find_packages(),
-     package_data={'pySpinW.swFuncs': [pathMySubPackage1data]},
-     classifiers=[
+    name='pySpinW',
+    version='0.1',
+    packages=find_packages(),
+    url='http://www.spinw.org',
+    license='MIT License',
+    author='Simon Ward',
+    author_email='simon.ward@esss.se',
+    description="Python implementation of SpinW",
+    classifiers=[
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          "Operating System :: OS Independent",
-     ],
-     include_package_data=True,
- )
+    ],
+    include_package_data=True,
+    install_requires=['numpy']
+)
